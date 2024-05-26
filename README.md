62d633d93940c7849b5b2e499014889af7bd9c03
= <=>
2024-05-26 14:14:20 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unfathom
+
+Get to the bottom of the ocean. How far can you make it before giving up?
+
+https://slickle.itch.io/unfathom
---
+++ game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var depth = 200
+
+func _physics_process(_delta):
+	depth += snapped(Input.get_action_strength("Go_Down") * .2, 0.1)
+	$DepthLabel.text = "Depth: %1d" % depth
+	$Background.position = Vector2(160, 5300 - depth)
+	if depth >= 10_000:
+		$DepthLabel.text = "You Win! Congrats."
+		depth = 10_000
+	if $Creature.is_visible():
+		$Creature.position.x += 1
+		if $Creature.position.x > 400:
+			$Creature.hide()
+	elif depth > 200 and randi_range(0, 1) == 0:
+		$Creature.frame = randi_range(0, 35)
+		$Creature.position = Vector2(-20, randi_range(50, 280))
+		$Creature.show()
