8cb85574e2a2e4d17184896a910e75d13b7dd90a
= <=>
2024-05-27 16:45:11 -0400

    changing back test props

--- game.gd
+++ game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var depth = 200
+var depth = 0
 
 func _physics_process(_delta):
 	depth += snapped(Input.get_action_strength("Go_Down") * .2, 0.1)
@@ -13,7 +13,7 @@ func _physics_process(_delta):
 		$Creature.position.x += 1
 		if $Creature.position.x > 400:
 			$Creature.hide()
-	elif depth > 200 and randi_range(0, 1) == 0:
+	elif depth > 200 and randi_range(0, 1000) == 0:
 		$Creature.frame = randi_range(0, 35)
 		$Creature.position = Vector2(-20, randi_range(50, 280))
 		$Creature.show()
